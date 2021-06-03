ca72fe38d66d1869afcded2afdf425c4aaea4c7b
Louis le gros <louis.capitaine@u-bordeaux.fr>
2021-06-03 02:50:56 +0200

    More tests

--- src/ExtraFrech.jl
+++ src/ExtraFrech.jl
@@ -1,5 +1,26 @@
 module ExtraFrech
 
 # Write your package code here.
+using Distances
+using Statistics
+using StatsBase
+using Random
+using ProgressMeter
+
+
+
+include("ERFRF.jl")
+include("ERtmax.jl")
+include("ERvar_split.jl")
+include("FRFERR.jl")
+include("Importance.jl")
+include("Impurity_split.jl")
+include("Impurity.jl")
+include("Pred_rf.jl")
+include("Pred_tree.jl")
+
+export impurity
+
+
 
 end
